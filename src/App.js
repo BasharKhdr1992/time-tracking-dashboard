8c8db16ae5d73c086b33c7a6740676f09fd92a13
bashar khdr <bashar.khdr.DS.92@hotmail.com>
2022-04-26 01:14:29 +0200

    adding attribution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import TimeDashboard from './components/TimeDashboard';
+import Appendix from './components/Appendix';
 
 const App = () => {
   return (
     <div className="wrapper">
       <TimeDashboard />
+      <Appendix />
     </div>
   );
 };
