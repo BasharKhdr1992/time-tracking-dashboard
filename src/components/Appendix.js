4107549dcf59e98dd16f379f1356a0c1f198b377
bashar khdr <bashar.khdr.DS.92@hotmail.com>
2022-04-26 04:37:48 +0200

    adding hover effect to activity card

--- src/components/Appendix.js
+++ src/components/Appendix.js
@@ -3,7 +3,7 @@ import './Appendix.css';
 
 const Appendix = () => {
   return (
-    <div class="attribution">
+    <div className="attribution">
       Challenge by{' '}
       <a
         href="https://www.frontendmentor.io?ref=challenge"
