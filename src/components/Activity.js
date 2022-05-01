a56b1b53fd42eab6e099e218d3a394c2195e2c07
bashar khdr <bashar.khdr.DS.92@hotmail.com>
2022-05-01 16:19:33 +0200

    updated the responsive design, and added animation to the dashboard

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import assets from '../assets';
 import './Activity.css';
 
 const Activity = ({ title, current, previous, bgColor, bgImage }) => {
+  const [currentCounter, setCurrentCounter] = useState(0);
+  const [prevCounter, setPrevCounter] = useState(0);
+
+  useEffect(() => {
+    setCurrentCounter(0);
+    setPrevCounter(0);
+
+    const x = setInterval(() => {
+      setCurrentCounter((prev) => {
+        if (prev < current) return prev + 1;
+        else return prev;
+      });
+      setPrevCounter((prev) => {
+        if (prev < previous) return prev + 1;
+        else return prev;
+      });
+    }, 100);
+
+    return () => {
+      clearInterval(x);
+    };
+  }, [current, previous]);
+
   return (
     <div
       className="activity"
@@ -18,8 +41,8 @@ const Activity = ({ title, current, previous, bgColor, bgImage }) => {
           <img src={assets.ellipsis} className="ellipsis" alt="ellipsis" />
         </div>
         <div className="activity-stats">
-          <div className="current">{`${current} hrs`}</div>
-          <div className="previous">Last week - {`${previous} hrs`}</div>
+          <div className="current">{`${currentCounter} hrs`}</div>
+          <div className="previous">Last week - {`${prevCounter} hrs`}</div>
         </div>
       </div>
     </div>
